d30995e63863852d27a80033ba0195c738306aa3
Gopu <qualitygopu@gmail.com>
2021-10-06 16:54:44 +0530

    ok

--- TimerSwitch/TimerSwitch.ino
+++ TimerSwitch/TimerSwitch.ino
@@ -1,48 +1,189 @@
-#include <Wire.h>
 #include "RTClib.h"
 #include <LiquidCrystal_I2C.h>
-#include <IRremote.h>
-#include <EEPROM.h>
+#include "LcdKeypad.h"
+#include "MenuManager.h"
+#include "MenuData.h"
+#include "Config.h"
 
-#define UP 16736925
-#define DOWN 16754775
-#define RIGHT 16761405
-#define LEFT 16720605
-#define SELECT 16712445
-#define SETTING 16732845
-#define AMP A3
+#define LOAD A3
 
 LiquidCrystal_I2C lcd(0x27, 16, 2);
-IRrecv irrecv(11);
-decode_results results;
-RTC_DS1307 rtc;
+RTC_DS3231 rtc;
+unsigned long timrAmp, timrLCD, timr_Time, timrMNU;
+MenuManager Menu1(sampleMenu_Root, menuCount(sampleMenu_Root));
+Config config;
+byte btn;
+char strbuf[LCD_COLS + 1];
+
+String months[12] = {"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"};
+
+enum AppModeValues
+{
+    APP_NORMAL_MODE,
+    APP_MENU_MODE,
+    APP_PROCESS_MENU_CMD
+};
+byte appMode = APP_NORMAL_MODE;
+
+void refreshMenuDisplay(byte refreshMode);
+byte getNavAction();
+byte processMenuCommand(byte cmdId);
+void showTime();
 
 void setup()
 {
-    EEPROM.begin();
-    irrecv.enableIRIn();
+    pinMode(LOAD, OUTPUT);
     lcd.init();
     lcd.backlight();
-    if (!rtc.begin())
+    lcd.setCursor(5, 0);
+    lcd.print(F("WELCOME"));
+    lcd.setCursor(6, 1);
+    lcd.print(F("QTRON"));
+    delay(2000);
+    lcd.clear();
+    lcd.setCursor(3, 0);
+    lcd.print("Loading...");
+    config.load();
+    while (!rtc.begin())
     {
         lcd.clear();
         lcd.print(F("ERR 01")); // Couldn't find RTC
-        abort();
     }
-    if (!rtc.isrunning())
+
+    if (rtc.lostPower())
     {
         lcd.clear();
         lcd.print(F("ERR 02"));
         delay(2000);
     }
+    lcd.clear();
+    timrLCD = millis();
 }
 
 void loop()
 {
-    if (irrecv.decode(&results))
+    btn = getButton();
+    if (btn)
+        {
+          timrLCD = millis();
+          timrMNU = millis();
+          lcd.backlight();
+        }
+    switch (appMode)
     {
-        Serial.println(results.value);
-        delay(200);
-        irrecv.resume();
+    case APP_NORMAL_MODE:
+        if (btn == BUTTON_SELECT_LONG_PRESSED)
+        {
+            appMode = APP_MENU_MODE;
+            refreshMenuDisplay(REFRESH_DESCEND);
+            timrMNU = millis();
+        }
+        if (rtc.now().minute() == 0 && rtc.now().second() == 0)
+        {
+            int hr = rtc.now().hour();
+            if (hr >= config.startTime && hr <= config.endTime) // chek time
+            {
+                digitalWrite(AMP, HIGH);
+                timrAmp = millis();
+                appMode = APP_CHANT_MODE;
+                lcd.clear();
+            }
+        }
+        if (millis() - timr_Time > 1000)
+        {
+            timr_Time = millis();
+            showTime();
+        }
+        if (millis() - timrLCD > (config.backLightDur * 1000L))
+        {
+            lcd.noBacklight();
+        }
+        break;
+    case APP_CHANT_MODE:
+    {
+        lcd.backlight();
+        lcd.setCursor(0,1);
+        lcd.print("CHANT PLAYING...");
+        if (millis() >= timrAmp + ampDelay)
+            PlayChant(rtc.now().hour());
+    }
+    case APP_MENU_MODE:
+    {
+        if (millis() - timrMNU > 10000)
+        {
+          Menu1.reset();
+          appMode = APP_NORMAL_MODE;
+          lcd.clear();
+        }
+           
+        if (Menu1.getCurrentItemCmdId() == 1)
+        {
+            StDate = rtc.now().day();
+            StMonth = rtc.now().month();
+            StYear = rtc.now().year();
+            StHr = rtc.now().hour();
+            StMin = rtc.now().minute();
+        }
+        if (btn == BUTTON_LEFT_LONG_PRESSED)
+        {
+            Menu1.reset();
+            appMode = APP_NORMAL_MODE;
+            lcd.clear();
+        }
+        byte menuMode = Menu1.handleNavigation(getNavAction, refreshMenuDisplay);
+
+        if (menuMode == MENU_EXIT)
+        {
+            lcd.clear();
+            appMode = APP_NORMAL_MODE;
+        }
+        else if (menuMode == MENU_INVOKE_ITEM)
+        {
+            appMode = APP_PROCESS_MENU_CMD;
+            if (Menu1.getCurrentItemCmdId())
+            {
+                lcd.setCursor(0, 1);
+                strbuf[0] = 0b01111110; // forward arrow representing input prompt.
+                strbuf[1] = 0;
+                lcd.print(strbuf);
+            }
+        }
+        break;
+    }
+    case APP_PROCESS_MENU_CMD:
+    {
+        byte processingComplete = processMenuCommand(Menu1.getCurrentItemCmdId());
+        if (millis() - timrMNU > 10000)
+        {
+          Menu1.reset();
+          appMode = APP_NORMAL_MODE;
+          lcd.clear();
+        }      
+        if (processingComplete)
+        {
+            appMode = APP_MENU_MODE;
+            // clear forward arrow
+            lcd.setCursor(0, 1);
+            strbuf[0] = ' '; // clear forward arrow
+            strbuf[1] = 0;
+            lcd.print(strbuf);
+        }
+        break;
+    }
+    case APP_DEMO_MODE:
+    {
+        lcd.setCursor(0, 0);
+        lcd.print(F("  RUNNING DEMO  "));
+        lcd.setCursor(0, 1);
+        lcd.print("DEMO TIME : " + String(demoTime) + String(demoTime >= 12 ? " PM" : " AM"));
+        digitalWrite(AMP, HIGH);
+        delay(1000);
+        PlayChant(demoTime);
+        if (btn == BUTTON_DOWN_PRESSED)
+        {
+          playSong = 9;
+          myDFPlayer.stop();
+        }
+    }
     }
 }
